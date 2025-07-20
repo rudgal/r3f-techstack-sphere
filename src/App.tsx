8e1232679ac48e46fd3b183d9e65b4ff18f24358
rudgal <office@galler.digital>
2025-07-20 16:40:59 +0200

    install r3f

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import './App.css';
+import { Canvas } from '@react-three/fiber';
 
 function App() {
   return (
-    <>
-      <div>
-        <p>Hello World!</p>
-      </div>
-    </>
+    <Canvas>
+      <mesh>
+        <boxGeometry args={[2, 2, 2]} />
+        <meshPhongMaterial />
+      </mesh>
+      <ambientLight intensity={0.1} />
+      <directionalLight position={[0, 0, 5]} color="red" />
+    </Canvas>
   );
 }
 
