512f18c0e669262cdc520318e93d6908aed7375f
rudgal <office@galler.digital>
2025-07-28 09:56:20 +0200

    add react StrictMode

--- src/App.tsx
+++ src/App.tsx
@@ -3,34 +3,37 @@ import { Canvas } from '@react-three/fiber';
 import * as THREE from 'three';
 import Experience from './Experience';
 import { Perf } from 'r3f-perf';
+import { StrictMode } from 'react';
 
 function App() {
   return (
-    <Canvas
-      // orthographic
-      // flat
-      dpr={[1, 2]}
-      gl={{
-        // alpha: false,
-        // antialias: false,
-        // stencil: false,
-        // depth: false,
-        // powerPreference: "high-performance",
-        // preserveDrawingBuffer: true,
-        toneMapping: THREE.CineonToneMapping,
-        outputColorSpace: THREE.SRGBColorSpace,
-      }}
-      camera={{
-        fov: 55,
-        // zoom: 100,
-        near: 0.1,
-        far: 200,
-        position: [3, 2, 6],
-      }}
-    >
-      <Perf showGraph={true} chart={{ hz: 60, length: 240 }} />
-      <Experience />
-    </Canvas>
+    <StrictMode>
+      <Canvas
+        // orthographic
+        // flat
+        dpr={[1, 2]}
+        gl={{
+          // alpha: false,
+          // antialias: false,
+          // stencil: false,
+          // depth: false,
+          // powerPreference: "high-performance",
+          // preserveDrawingBuffer: true,
+          toneMapping: THREE.CineonToneMapping,
+          outputColorSpace: THREE.SRGBColorSpace,
+        }}
+        camera={{
+          fov: 55,
+          // zoom: 100,
+          near: 0.1,
+          far: 200,
+          position: [3, 2, 6],
+        }}
+      >
+        <Perf showGraph={true} chart={{ hz: 60, length: 240 }} />
+        <Experience />
+      </Canvas>
+    </StrictMode>
   );
 }
 
